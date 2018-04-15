04ee3bbc83fe1516a856ab3e80a8d1cf520929ef
chentyson <1716677@qq.com>
2018-04-15 11:19:19 +0800

    fix

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -6,4 +6,8 @@ systemctl daemon-reload
 systemctl stop supervisord
 systemctl disable supervisord
 systemctl restart nginx
+firewall-cmd --remove-port 11000-20000/tcp --permanent
+firewall-cmd --remove-port 11000-20000/udp --permanent
+firewall-cmd --add-port 11000-50000/tcp --permanent
+firewall-cmd --add-port 11000-50000/udp --permanent
 
