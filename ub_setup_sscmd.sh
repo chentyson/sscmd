acc1209770504c4d05088fc04410204a01129cfd
chentyson <1716677@qq.com>
2018-05-08 03:59:28 -0700

    fix

--- ub_setup_sscmd.sh
+++ ub_setup_sscmd.sh
@@ -1,6 +1,6 @@
 #install ss-go
 chmod +x shadowsocks-go.sh
-./shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
+./shadowsocks-go.sh
 /etc/init.d/shadowsocks stop
 chmod -x /etc/init.d/shadowsocks
 cp ./config.json /etc/shadowsocks/config.json
