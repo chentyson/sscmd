b5cfb5fa21d5da23ae67aa4bebd2ccdf582120ab
chentyson <1716677@qq.com>
2018-05-12 19:23:15 -0700

    fix

--- syndb.sh
+++ syndb.sh
@@ -1,3 +1,3 @@
 mv /etc/shadowsocks/sscmd.db /etc/shadowsocks/sscmd.db.$(date +%Y%m%d_%H%m%s)
-scp root@$1.boosoo.cn:/etc/shadowsocks/sscmd.db ./
+scp root@$1.boosoo.cn:/etc/shadowsocks/sscmd.db /etc/shadowsocks/sscmd.db
 supervisorctl restart ssrun
