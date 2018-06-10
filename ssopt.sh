21d62248c87bac658898490376040330aa14523a
chentyson <1716677@qq.com>
2018-06-09 18:39:18 -0700

    add port rangs

--- ssopt.sh
+++ ssopt.sh
@@ -14,38 +14,38 @@ sed -i '$a \/sbin\/modprobe tcp_hybla' /etc/rc.d/rc.local
 chmod +x /etc/rc.d/rc.local
 
 #update sysctl.conf
-sed -i '/fs.file-max = 51200/d' /etc/sysctl.conf
+sed -i '/fs.file-max/d' /etc/sysctl.conf
 sed -i '$a fs.file-max = 51200' /etc/sysctl.conf
 
-sed -i '/net.core.wmem_max = 67108864/d' /etc/sysctl.conf
+sed -i '/net.core.wmem_max/d' /etc/sysctl.conf
 sed -i '$a net.core.wmem_max = 67108864' /etc/sysctl.conf
 
-sed -i '/net.core.netdev_max_backlog = 250000/d' /etc/sysctl.conf
+sed -i '/net.core.netdev_max_backlog/d' /etc/sysctl.conf
 sed -i '$a net.core.netdev_max_backlog = 250000' /etc/sysctl.conf
 
-sed -i '/net.core.somaxconn = 3240000/d' /etc/sysctl.conf
+sed -i '/net.core.somaxconn/d' /etc/sysctl.conf
 sed -i '$a net.core.somaxconn = 3240000' /etc/sysctl.conf
 
-sed -i '/net.ipv4.tcp_syncookies = 1/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_syncookies/d' /etc/sysctl.conf
 sed -i '$a net.ipv4.tcp_syncookies = 1' /etc/sysctl.conf
 
-sed -i '/net.ipv4.tcp_tw_reuse = 1/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_tw_reuse/d' /etc/sysctl.conf
 sed -i '$a net.ipv4.tcp_tw_reuse = 1' /etc/sysctl.conf
 
-sed -i '/net.ipv4.tcp_tw_recycle = 0/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_tw_recycle/d' /etc/sysctl.conf
 sed -i '$a net.ipv4.tcp_tw_recycle = 0' /etc/sysctl.conf
 
-sed -i '/net.ipv4.tcp_fin_timeout = 30/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_fin_timeout/d' /etc/sysctl.conf
 sed -i '$a net.ipv4.tcp_fin_timeout = 30' /etc/sysctl.conf
 
-sed -i '/net.ipv4.tcp_keepalive_time = 1200/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_keepalive_time/d' /etc/sysctl.conf
 sed -i '$a net.ipv4.tcp_keepalive_time = 1200' /etc/sysctl.conf
 
-sed -i '/net.ipv4.ip_local_port_range = 10000 65000/d' /etc/sysctl.conf
-sed -i '$a net.ipv4.ip_local_port_range = 10000 65000' /etc/sysctl.conf
+sed -i '/net.ipv4.ip_local_port_range/d' /etc/sysctl.conf
+sed -i '$a net.ipv4.ip_local_port_range = 8000 65000' /etc/sysctl.conf
 
 sed -i '/net.ipv4.ip_local_reserved_ports/d' /etc/sysctl.conf
-sed -i '$a net.ipv4.ip_local_reserved_ports = 11000-11999,31000-31999' /etc/sysctl.conf
+sed -i '$a net.ipv4.ip_local_reserved_ports = 11000-11300,12000-12300,13000-13300,14000-14300,21000-21300,22000-22300,23000-23300,24000-24300,25000-25300,32000-32300,41000-42000,52000-52300' /etc/sysctl.conf
 
 sed -i '/net.ipv4.tcp_max_syn_backlog = 8192/d' /etc/sysctl.conf
 sed -i '$a net.ipv4.tcp_max_syn_backlog = 8192' /etc/sysctl.conf
