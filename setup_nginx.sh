f4681efa4236e8c532da8cdb28e2c047ce3deebb
chentyson <1716677@qq.com>
2018-07-29 05:53:47 -0700

    fix

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -3,11 +3,13 @@ yum install nginx -y
 cp ./nginx.conf /etc/nginx/nginx.conf
 cp ./nginx.service /usr/lib/systemd/system/nginx.service
 systemctl daemon-reload
-systemctl stop supervisord
-systemctl disable supervisord
-systemctl restart nginx
+#systemctl stop supervisord
+#systemctl disable supervisord
+#systemctl restart nginx
 firewall-cmd --remove-port 11000-20000/tcp --permanent
 firewall-cmd --remove-port 11000-20000/udp --permanent
-firewall-cmd --add-port 11000-50000/tcp --permanent
-firewall-cmd --add-port 11000-50000/udp --permanent
-
+firewall-cmd --add-port 11000-65000/tcp --permanent
+firewall-cmd --add-port 11000-65000/udp --permanent
+firewall-cmd --add-port 22/tcp --permanent
+firewall-cmd --reload
+firewall-cmd --list-port
