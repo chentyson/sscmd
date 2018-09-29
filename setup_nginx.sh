cbec70f40f111ab3e28aebc17e210fb074a67e18
chentyson <1716677@qq.com>
2018-09-29 07:40:52 -0700

    fix

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -1,6 +1,6 @@
 yum install epel-release -y   #so can install nginx
 yum install nginx -y
-cp ./nginx.conf /etc/nginx/nginx.conf
+#cp ./nginx.conf /etc/nginx/nginx.conf
 cp ./nginx.service /usr/lib/systemd/system/nginx.service
 systemctl daemon-reload
 #systemctl stop supervisord
